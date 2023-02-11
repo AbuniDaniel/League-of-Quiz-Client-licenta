297ea16b2e7e202773c5655ef69915dd621e3e44
AbuniDaniel <daniel.abuni@s.unibuc.ro>
2023-02-11 23:19:43 +0200

    tot asa

--- src/pages/multiplayer/multiplayer.js
+++ src/pages/multiplayer/multiplayer.js
@@ -3,7 +3,6 @@ import "./multiplayer.css";
 import Axios from "axios";
 import { useState, useEffect, useContext } from "react";
 import io from "socket.io-client";
-import answer_button from "../play/answer-button.png"
 import { CountdownCircleTimer } from 'react-countdown-circle-timer'
 import { notification } from 'antd';
 import "antd/lib/notification/style/index.css";
@@ -17,8 +16,8 @@ function Multiplayer() {
     const [filter, setFilter] = useState('');
     const [isVisible, setIsVisible] = useState(false);
     const [isVisible22, setIsVisible22] = useState([false, false, false, false]);
+    const [isVisible44, setIsVisible44] = useState([false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]);
     const [key, setKey] = useState(0);
-    const [isButtonDisabled, setIsButtonDisabled] = useState(false);
     useEffect(() => {
       const fetchChampions = async() =>{
         const response = await Axios.get("https://daniel-licenta-api.herokuapp.com/champion-options")
@@ -71,12 +70,19 @@ function Multiplayer() {
     });
 
     socket.on('newChampionVisible', (data) => {
-      setIsVisible22(data)
+      if(data.length === 4)
+        setIsVisible22(data)
+      else
+        setIsVisible44(data)
       setGoodToChoose(false)
     })
 
     socket.on('newChampionNewGame', (data) => {
-      setIsVisible22(data.visibilityData)
+      console.log(data.visibilityData)
+      if(data.visibilityData.length === 4)
+        setIsVisible22(data.visibilityData)
+      else
+        setIsVisible44(data.visibilityData)
       setChampion(data.champion.championSelectedSrc)
       setDificulty(data.champion.dificulty)
       setKey(prevKey => prevKey + 1)
@@ -94,6 +100,19 @@ function Multiplayer() {
         message: data.error,
       });
     });
+
+    socket.on('correctAnswerP1', (data) => {
+      setFilter('');
+      notification['success']({
+        message: data.text,
+      });
+    });
+
+    socket.on('correctAnswerP2', (data) => {
+      notification['success']({
+        message: data.text,
+      });
+    });
     return () => {
       socket.disconnect();
     };
@@ -108,7 +127,7 @@ function Multiplayer() {
   };
 
   const gameInProgress = () => {
-    socket.emit('gameInProgress', {roomCode: roomCode, goodToChoose: goodToChoose})
+    socket.emit('gameInProgress', {roomCode: roomCode, goodToChoose: goodToChoose, dificulty: dificulty});
   }
 
   const newChampion = () => {
@@ -178,9 +197,7 @@ function Multiplayer() {
         <div className="wrapper">
       <div className="answers-list">
       <div className="answers">
-        {/* <div className="hint">
-        <p className="hint-text"><FlipNumbers height={16} width={12} play={true} duration={1} perspective={70} numbers={`${hints}`} /></p>
-        </div> */}
+
         <input id="filter"
           placeholder="Type champion name..."
           name="filter"
@@ -191,7 +208,6 @@ function Multiplayer() {
           onFocus={() => setIsVisible(true)}
         />
         
-        <button className="guess-button" style={{filter: isButtonDisabled ? 'brightness(70%)' : ''}} disabled={isButtonDisabled}><img src={answer_button} alt="answer_button"/></button>
       </div>
 
       <ul>
@@ -210,6 +226,77 @@ function Multiplayer() {
         <div className="gridimg" ><img className="img2" style={{visibility: isVisible22[1] ? 'visible' : 'hidden'}} src={champion} alt="loading champion"/></div>
         <div className="gridimg" ><img className="img3" style={{visibility: isVisible22[2] ? 'visible' : 'hidden'}} src={champion} alt="loading champion"/></div>
         <div className="gridimg" ><img className="img4" style={{visibility: isVisible22[3] ? 'visible' : 'hidden'}} src={champion} alt="loading champion"/></div>
+      </div>
+    </div>
+      </div>
+    );
+  }
+  else if(mode === "playing" && (dificulty === "easy44"|| dificulty === "hard44")){
+    gameInProgress();
+    return (
+      <div>
+        <CountdownCircleTimer
+        key={key}
+    isPlaying
+    duration={1}
+    colors={['#004777', '#F7B801', '#A30000', '#A30000']}
+    colorsTime={[5, 4, 2, 0]}
+    onComplete={() => {
+      if(allTrue(isVisible44)){
+        setIsVisible44([false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false])
+        newChampion();
+      }
+      setGoodToChoose(true);
+      return { shouldRepeat: true} // repeat animation in 1.5 seconds
+    }}
+  >
+    {({ remainingTime }) => remainingTime}
+  </CountdownCircleTimer>
+        <p>Room code: {roomCode}</p>
+        <p>Game started</p>
+        <div className="wrapper">
+      <div className="answers-list">
+      <div className="answers">
+        <input id="filter"
+          placeholder="Type champion name..."
+          name="filter"
+          type="textt"
+          value={filter}
+          onChange={event => setFilter(event.target.value)}
+          autoComplete = "off"
+          onFocus={() => setIsVisible(true)}
+        />
+        
+      </div>
+
+      <ul>
+      <div className="lista"
+      style={{
+        visibility: isVisible ? 'visible' : 'hidden'
+      }} >
+      {champions?.filter(f => f.startsWith(filter.charAt(0).toUpperCase() + filter.slice(1).toLowerCase()) && filter !== '')
+        .map(f =><button key={f} onClick={() => {checkAnswer(f); setIsVisible(false)}} className="guess-word" ><span>{f}</span> </button>)}
+      </div>
+      </ul>
+      </div>
+
+      <div className="grid44" style={{pointerEvents: 'none'}}>
+        <div className="gridimg44" ><img className="img144" style={{visibility: isVisible44[0] ? 'visible' : 'hidden'}} src={champion} alt="loading champion"/></div>
+        <div className="gridimg44" ><img className="img244" style={{visibility: isVisible44[1] ? 'visible' : 'hidden'}} src={champion} alt="loading champion"/></div>
+        <div className="gridimg44" ><img className="img344" style={{visibility: isVisible44[2] ? 'visible' : 'hidden'}} src={champion} alt="loading champion"/></div>
+        <div className="gridimg44" ><img className="img444" style={{visibility: isVisible44[3] ? 'visible' : 'hidden'}} src={champion} alt="loading champion"/></div>
+        <div className="gridimg44" ><img className="img544" style={{visibility: isVisible44[4] ? 'visible' : 'hidden'}} src={champion} alt="loading champion"/></div>
+        <div className="gridimg44" ><img className="img644" style={{visibility: isVisible44[5] ? 'visible' : 'hidden'}} src={champion} alt="loading champion"/></div>
+        <div className="gridimg44" ><img className="img744" style={{visibility: isVisible44[6] ? 'visible' : 'hidden'}} src={champion} alt="loading champion"/></div>
+        <div className="gridimg44" ><img className="img844" style={{visibility: isVisible44[7] ? 'visible' : 'hidden'}} src={champion} alt="loading champion"/></div>
+        <div className="gridimg44" ><img className="img944" style={{visibility: isVisible44[8] ? 'visible' : 'hidden'}} src={champion} alt="loading champion"/></div>
+        <div className="gridimg44" ><img className="img1044" style={{visibility: isVisible44[9] ? 'visible' : 'hidden'}} src={champion} alt="loading champion"/></div>
+        <div className="gridimg44" ><img className="img1144" style={{visibility: isVisible44[10] ? 'visible' : 'hidden'}} src={champion} alt="loading champion"/></div>
+        <div className="gridimg44" ><img className="img1244" style={{visibility: isVisible44[11] ? 'visible' : 'hidden'}} src={champion} alt="loading champion"/></div>
+        <div className="gridimg44" ><img className="img1344" style={{visibility: isVisible44[12] ? 'visible' : 'hidden'}} src={champion} alt="loading champion"/></div>
+        <div className="gridimg44" ><img className="img1444" style={{visibility: isVisible44[13] ? 'visible' : 'hidden'}} src={champion} alt="loading champion"/></div>
+        <div className="gridimg44" ><img className="img1544" style={{visibility: isVisible44[14] ? 'visible' : 'hidden'}} src={champion} alt="loading champion"/></div>
+        <div className="gridimg44" ><img className="img1644" style={{visibility: isVisible44[15] ? 'visible' : 'hidden'}} src={champion} alt="loading champion"/></div>
       </div>
     </div>
       </div>
